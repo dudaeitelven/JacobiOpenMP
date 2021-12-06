db99385e9b38ab0a918dd01955debbf5b62af805
dudaeitelven <32472327+dudaeitelven@users.noreply.github.com>
2021-12-06 15:50:05 -0300

    First
    First

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+#pragma pack(1)
+
+#define ITERACOES = 1000;
+#define ERRO = 1e-10;
+
+/*
+Para compilar:
+1 - Abrir o local do fonte
+2 - Digitar para compilar: gcc -o <programa> <programa.c> -fopenmp
+3 - Digitar para rodar: ./<Programa> <imagem_entrada> <imagem_saida> <mascara> <numero_threads>
+*/
+
+int main(int argc, char **argv ){
+
+}
